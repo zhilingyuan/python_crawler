0e64f2fc90986d22229dc27d55fe8803c669e179
zhilingyuan <33947979+zhilingyuan@users.noreply.github.com>
2018-01-23 00:26:39 +0800

    learn
    learn

--- crawl_content.py
+++ crawl_content.py
@@ -2,14 +2,26 @@
 import urllib.request
 import re
 headers=('User-Agent','Mozilla/5.0 (Windows NT 6.1;WOW64) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/28.0.2125.122 Safari/537.36 SE 2.X MetaSr 1.0')
-url='http://qiushibaike.com/8hr/page/1'
+url='http://qiushibaike.com/8hr/page/2'
 opener=urllib.request.build_opener()
 opener.addheaders=[headers]
 urllib.request.install_opener(opener)
-data=urllib.request.urlopen(url).decode('utf-8')
+data=urllib.request.urlopen(url).read().decode('utf-8')
 userpat='target="_blank" title="(.*?)">'
 contentpat='<div class="content">(.*?)</div>'
 userlist=re.compile(userpat,re.S).findall(data)
 contentlist=re.compile(contentpat,re.S).findall(data)
-num=1
-for 
+numx=1
+for content in contentlist:
+    content=content.replace('\n','')
+    name='content'+str(numx)
+    exec(name+'=content')
+    numx+=1
+numy=1
+for user in userlist:
+    name='content'+str(numy)
+    print('user'+str(numy)+':'+user)
+    print('content:')
+    exec('print('+name+')')
+    print('\n')
+    numy+=1
