0bc082ca80ebf66ef49c729f470107bb3a21850e
pixiaoyuan <33947979+pixiaoyuan@users.noreply.github.com>
2018-02-28 16:25:03 +0800

    learn
    learn

---
+++ scrapy/mzitu/begin.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/10/30 19:28
+# @Author  : Yu
+# @Site    : 
+# @File    : begin.py
+from scrapy import cmdline
+cmdline.execute(["scrapy","crawl","meizi"])
--- scrapy/qtpjt/qtpjt/spiders/qtspd.py
+++ scrapy/qtpjt/qtpjt/spiders/qtspd.py
@@ -10,13 +10,14 @@ class QtspdSpider(scrapy.Spider):
     start_urls = ['https://list.jd.com/list.html?cat=670%2C671%2C672&go=0']
     
     def parse(self, response):
-        from scrapy.shell import inspect_response
-        inspect_response(response,self)
+        #from scrapy.shell import inspect_response
+        #inspect_response(response,self)
         
         item=QtpjtItem()
         paturl='<img width="220" height="220" data-img="1" data-lazy-img="//(.+?\.jpg)'
         item["picurl"]=re.compile(paturl).findall(str(response.body))
         item["picid"]=re.compile(paturl).findall(str(response.body))
+        yield item#忘记这一句会使得item 为空
         for i in range(2,4):
             nexturl='https://list.jd.com/list.html?cat=670%2C671%2C672&go='+str(i)
             yield Request(nexturl,callback=self.parse)#为什么会为空 items
